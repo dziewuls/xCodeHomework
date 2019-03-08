66a598939274967ce33f53f0f2ce892259b4212a
Maćki Ido <maciej.macko888@gmail.com>
2019-03-08 14:32:45 +0100

    generate homework-back app

---
+++ homework-back/src/main/java/com/xcode/homeworkback/HomeworkBackApplication.java
@@ -0,0 +1,13 @@
+package com.xcode.homeworkback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkBackApplication.class, args);
+	}
+
+}
